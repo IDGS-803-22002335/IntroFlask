9c5cd1fc01a1280a3bc3380476476aa2ba6b246e
IDGS-803-22002335 <tovardiazjuanaesmeralda@gmail.com>
2025-01-27 22:24:13 -0600

    Apis

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
 from flask import Flask
 
-
-app = Flask(__name__)
-
+app=Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hola mundo!!"
 
-
 @app.route("/hola")
 def hola():
     return "Hola!!!"
-
-# va a recibir un parametro de tipo String
-
 
 @app.route("/user/<string:user>")
 def user(user):
     return f"Hola {user}!!!"
 
-
 @app.route("/numero/<int:n>")
 def numero(n):
     return "Numero {}".format(n)
 
-# los metodos tiene que cambiar si una ruta es igual para dos métodos
-
-
 @app.route("/user/<string:user>/<int:id>")
-def username(user, id):
+def username(user,id):
     return f"Nombre: {user} ID: {id}!!!"
 
-# pasando dos número flotantes para regresar la suma
-
-
 @app.route("/suma/<float:n1>/<float:n2>")
-def suma(n1, n2):
+def suma(n1,n2):
     return "La suma es: {}!!!".format(n1+n2)
+
+@app.route("/default")
+@app.route("/default/<string:nom>")
+def func(nom="pedro"):
+    return "El nombre de Nom es "+nom
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=300)
+    app.run(debug=True,port=300)
